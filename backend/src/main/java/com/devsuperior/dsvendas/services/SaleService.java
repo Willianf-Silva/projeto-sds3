1c4e7838f50c4cfacb139951fa5cf71d7d721eac
Willianf-Silva <willian.ferreira.da.silva@gmail.com>
2021-05-05 22:42:54 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dsvendas.services;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface SaleService {
     Page<SaleDTO> findAll(Pageable pageable);
+    List<SaleSumDTO> amountGroupedBySeller();
+    List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/devsuperior/dsvendas/services/impl/SaleServiceImpl.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/impl/SaleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsvendas.services.impl;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
 import com.devsuperior.dsvendas.services.SaleService;
@@ -11,6 +13,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class SaleServiceImpl implements SaleService {
 
@@ -20,10 +24,23 @@ public class SaleServiceImpl implements SaleService {
     @Autowired
     private SellerService sellerService;
 
+    @Override
     @Transactional(readOnly = true)
     public Page<SaleDTO> findAll(Pageable pageable){
         sellerService.findAll();
         Page<Sale> result = saleRepository.findAll(pageable);
         return result.map(x -> new SaleDTO(x));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<SaleSumDTO> amountGroupedBySeller() {
+        return saleRepository.amountGroupedBySeller();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<SaleSuccessDTO> successGroupedBySeller() {
+        return saleRepository.successGroupedBySeller();
     }
 }
